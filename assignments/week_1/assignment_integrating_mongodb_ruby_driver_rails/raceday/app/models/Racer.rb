9f1daace30eea430dc6582b79344551ef9f0c6fd
Kathryn Tipton <kathryn.tipton@gmail.com>
2017-03-27 09:35:11 -0400

    Process on Week 1 Assignment

--- assignments/week_1/assignment_integrating_mongodb_ruby_driver_rails/raceday/app/models/Racer.rb
+++ assignments/week_1/assignment_integrating_mongodb_ruby_driver_rails/raceday/app/models/Racer.rb
@@ -1,4 +1,19 @@
 class Racer
+  attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
+
+#  @id=doc[:_id].to_s
+#  :_id=>BSON::ObjectId.from_string(@id))
+#  # initialize from both a Mongo and Web hash
+  def initialize(params={})
+    #switch between both internal and external views of id
+    @id=params[:_id].nil? ? params[:id].to_s : params[:_id].to_s
+    @number=params[:number]
+    @first_name=params[:first_name]
+    @last_name=params[:last_name]
+    @gender=params[:gender]
+    @group=params[:group]
+    @secs=params[:secs]
+  end
 
   # convenience method of access to client in console
   def self.mongo_client
@@ -10,15 +25,22 @@ class Racer
     self.mongo_client['racers']
   end
 
-  def self.all(prototype={}, sort={:number=>1}, offset=0, limit=100)
-    prototype=prototype.symbolize_keys.slice(:number, :first_name, :last_name, :gender, :group, :secs) if !prototype.nil?
+  def self.all(prototype={}, sort={:number=>1}, offset=0, limit=nil)
     result=collection.find(prototype)
           .projection({_id:true, number:true, first_name:true, last_name:true, gender:true, group:true, secs:true})
           .sort(sort)
           .skip(offset)
-    result=result.limit(limit) if !limit.nil?
 
+    result=result.limit(limit) if !limit.nil?
     return result
   end
+
+  def self.find id
+
+    doc=collection.find(:_id=>id).first
+    return doc.nil? ? nil : Zip.new(doc)
+  end 
+
+
 end
 
