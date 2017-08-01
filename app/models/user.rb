cf31945937a7f225751db9908713cbd1710d8c65
Kathryn Tipton <kathryn.tipton@gmail.com>
2017-08-01 16:13:35 -0400

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 12 }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 12 }, allow_nil: true
 
 
   # Returns the hash digest of the given string.
