ae2d3834de13396d2f9fa712d67942512a01fbdd
Kathryn Tipton <ktipton@usgcrp.gov>
2018-04-03 13:21:50 -0400

    Express router exercise finish. Assignnment outline

--- exercises/week_1/express-router/index.js
+++ exercises/week_1/express-router/index.js
@@ -2,6 +2,7 @@ const express = require('express'),
      http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -11,52 +12,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.all('/dishes', (req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    next(); //keep going to the next matching route
 
-});
-
-app.get('/dishes', (req,res,next) => {
-  res.end('Will send all the dishes to you!');
-  //does not continue on, as the .end was invoked
-});
-
-app.post('/dishes', (req,res,next) => {
-  res.end('Will add the dish: ' + req.body.name +
-      ' with details: ' + req.body.description);
-});
-
-app.put('/dishes', (req,res,next) => {
-  res.statusCode = 403;
-  res.end('PUT op not supported on /dishes');
-});
-
-app.delete('/dishes', (req,res,next) => {
-  res.end('Deleting all the dishes!');
-});
-
-app.get('/dishes/:dishId', (req,res,next) => {
-  res.end('Will send details the dish' +
-      req.params.dishId + ' to you!');
-});
-
-app.post('/dishes/:dishId', (req,res,next) => {
-  res.statusCode = 403;
-  res.end('POST op not supported on /dishes/' + req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req,res,next) => {
-  res.write('Updating the dish: ' + req.params.dishId + '\n');
-  res.end('Will update the dish: ' + req.body.name +
-      ' with details: ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req,res,next) => {
-  res.end('Deleting the dish' + req.params.dishId );
-});
-
+app.use('/dishes', dishRouter);
 app.use(express.static(__dirname + '/public'));
 
 
